5e2a464ea132b9cdde317973681217e072aef07d
prateeksachdeva <sachdeva852000@gmail.com>
2025-01-14 22:53:11 -0800

    First compose function with preview (with & without style)

--- app/src/main/java/com/psachdev/composeplayground/MainActivity.kt
+++ app/src/main/java/com/psachdev/composeplayground/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,15 +15,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            MessageCard("Android")
+            //Text("Hello world!")
+            /*Commenting out generated code
             ComposePlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
-            }
+            }*/
         }
+    }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard("Android")
     }
 }
 
