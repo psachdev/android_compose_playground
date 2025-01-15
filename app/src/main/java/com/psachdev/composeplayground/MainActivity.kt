f850580e1d711be078be3ab696eee44ce9ca238c
prateeksachdeva <sachdeva852000@gmail.com>
2025-01-14 23:05:59 -0800

    Using material design

--- app/src/main/java/com/psachdev/composeplayground/MainActivity.kt
+++ app/src/main/java/com/psachdev/composeplayground/MainActivity.kt
@@ -3,16 +3,17 @@ package com.psachdev.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,20 +26,13 @@ import com.psachdev.composeplayground.ui.theme.ComposePlaygroundTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            MessageCard(Message("Prateek", "Trying out Compose"))
-            //Text("Hello world!")
-            /*Commenting out generated code
             ComposePlaygroundTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
                 }
-            }*/
+            }
         }
     }
 }
@@ -74,22 +68,8 @@ fun MessageCard(msg: Message) {
 @Composable
 fun PreviewMessageCard() {
     ComposePlaygroundTheme {
-        MessageCard(Message("Prateek", "Trying out Compose"))
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposePlaygroundTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
     }
 }
